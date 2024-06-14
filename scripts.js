3466e51246be9e9a3c3492170222f1dba92e208d
boikanyobaitshepi <boikanyobaitshepi@gmail.com>
2024-06-14 11:40:55 +0200

    coomit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,26 @@
-// Add your code to add 2 numbers together
-function addNumbers(a, b) {
 
-}
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ 
+const nums = [2, 7, 11, 15];
+const target = 9;
 
-console.log(addNumbers(10, 20)); // Expected output: 30
+var twoSum = function(nums, target) {
+    const numMap = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+      const complement = target - nums[i];
+      if (numMap[complement] !== undefined) {
+        return [numMap[complement], i];
+      }
+      numMap[nums[i]] = i;
+    }
+    
+    return []; 
+  };
+  
+
+
