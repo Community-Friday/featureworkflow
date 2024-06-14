b6ba9bd26efc0f61ea7f26fd7e101637d637e8c3
SABELO MAWELA <MAWELAS63@GMAIL.COM>
2024-06-14 11:48:23 +0200

    addNumbers function to return sum of two numbers without adding 2

--- scripts.js
+++ scripts.js
@@ -1,34 +1,6 @@
-<<<<<<< HEAD
 
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
- 
-const nums = [2, 7, 11, 15];
-const target = 9;
-
-var twoSum = function(nums, target) {
-    const numMap = {};
-    
-    for (let i = 0; i < nums.length; i++) {
-      const complement = target - nums[i];
-      if (numMap[complement] !== undefined) {
-        return [numMap[complement], i];
-      }
-      numMap[nums[i]] = i;
-    }
-    
-    return []; 
-  };
-  
-
-
-=======
-// Add your code to add 2 numbers together
 function addNumbers(a, b) {
-  return a + b +2;
+  return a + b;
 }
 
 console.log(addNumbers(10, 20)); // Expected output: 30
