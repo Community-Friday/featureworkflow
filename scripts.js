dcdfa2f057d273a0a0d2ef344b8d040d9416cdd3
SibusisoNgubeni <156700279+SibusisoNgubeni@users.noreply.github.com>
2024-06-14 11:28:35 +0200

    solution

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
-// Add your code to add 2 numbers together
+
 function addNumbers(a, b) {
+    return a+b
 
 }
 
-console.log(addNumbers(10, 20)); // Expected output: 30
+console.log(addNumbers(10, 20)); 
