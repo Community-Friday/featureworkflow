fbc145a1b4dd67d0feeb114ac9e13ec6964be473
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-06-14 11:51:20 +0200

    returned code to original

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // Add your code to add 2 numbers together
 function addNumbers(a, b) {
-  return a + b +2;
+
 }
 
-console.log(addNumbers(10, 20)); // Expected output: 30
+console.log(addNumbers(10, 20)); // Expected output: 30 
