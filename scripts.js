3fca734e0afb05bbf22ce05057d82adaccd34b81
LebogangMputle <lebozikalala@gmail.com>
2024-06-14 11:43:48 +0200

    "created a for loop"

--- scripts.js
+++ scripts.js
@@ -1,6 +1,18 @@
-// Add your code to add 2 numbers together
-function addNumbers(a, b) {
+const nums = [2, 7, 11, 15];
+const target = 9;
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @returns {number[]} 
+ */
+function twoSum(nums, target) {
+  const seen = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+  }
 }
 
-console.log(addNumbers(10, 20)); // Expected output: 30
+const result = twoSum(nums, target);
+console.log(result);
